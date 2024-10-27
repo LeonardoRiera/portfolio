7e2f596a34bbd67cb1beb77294ea5e6c6362a97a
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2024-10-26 23:57:26 -0300

    efectos para section proyects

--- src/Components/Proyects/Proyects.jsx
+++ src/Components/Proyects/Proyects.jsx
@@ -29,8 +29,8 @@ const Proyects = () => {
   return (
     <div className='ProyectsContainer'>
 
-      {/* <h1 className='TituloPrincipal'>{t('header.titulo')}</h1> */}
-
+      
+      {/* tracker */}
       <div className='ProyectContainer'>
         
         <div className='image-container'>
@@ -41,14 +41,44 @@ const Proyects = () => {
 
         {/* Div que contiene la información del proyecto */}
         <div className="project-info">
-          <h3>Tracker</h3>
-          <p>This is a detailed description of the project. It will slide up when you hover over the images.</p>
+          <div>
+            <h3 className='proyectNameTracker'>Tracker</h3>
+            <p className='proyectDevs1'>{t('tracker.descripcion')}:</p>
+            <p className='proyectDevs'>{t('tracker.descripcionDev')} </p>
+            <p className='proyectDevs'>{t('tracker.flujo')}</p>
+            <p className='proyectDevs'>{t('tracker.relevancias')}</p>
+            <p className='proyectDevs'>{t('tracker.responsive')}</p>
+           
+          </div>
+          <hr />
+          <div className='travelTechContainer'>
+
+            <h5 className='techTitle'>{t('proyectos.tecnologias')}:</h5>
+
+            <div className='logosContainer'>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/sass/sass-original.svg " className='logos' />
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" className='logos' />
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" className='logos' />
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/photoshop/photoshop-original.svg" className='logos' />
+             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/illustrator/illustrator-plain.svg" className='logos' />
+    
+            </div>
+            
+
+          </div>
         </div>
 
+        <div className="project-infoBajo">
+          
+          <a href="https://github.com/LeonardoRiera/trackLanding.git" target='blank' className='gatoTracker'><i class="devicon-github-original gatoTracker"></i></a>
+          
+        </div>
         
-
       </div>
 
+
+
+      {/* imperio tickets */}
       <div className='ProyectContainer'>
         
         <div className='image-container'>
@@ -59,12 +89,48 @@ const Proyects = () => {
 
         {/* Div que contiene la información del proyecto */}
         <div className="project-info2">
-          <h3>Imperio Ticket</h3>
-          <p>This is a detailed description of the project. It will slide up when you hover over the images.</p>
+          <div>
+            <h3 className='proyectNameImperio'>Imperio Ticket</h3>
+            <p className='proyectDevs1'>{t('imperioTickets.descripcion')}:</p>
+            <p className='proyectDevs'>{t('imperioTickets.descripcionDev')} </p>
+            <p className='proyectDevs'>{t('imperioTickets.flujo')}</p>
+            <p className='proyectDevs'>{t('imperioTickets.relevancias')}</p>
+            <p className='proyectDevs'>{t('imperioTickets.responsive')}</p>
+          </div>
+          <hr />
+          <div className='travelTechContainer2'>
+
+            <h5 className='techTitle'>{t('proyectos.tecnologias')}:</h5>
+
+            <div className='logosContainer3'>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" className='logos'/>
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" className='logos' />
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" className='logos' />
+              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/firebase/firebase-original.svg" className='logos' />
+            </div>
+            <div className='logosContainer4'>  
+             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/photoshop/photoshop-original.svg" className='logos' />
+             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/illustrator/illustrator-plain.svg" className='logos' />
+           
+            </div>
+
+          </div>
+
         </div>
+
+        <div className="project-infoBajo">
+          
+          <a href="https://github.com/LeonardoRiera/IMPERIOtickets.git" target='blank' className='gatoImperio'><i class="devicon-github-original gatoImperio"></i></a>
+          
+        </div>
+        
 
       </div>
 
+
+
+
+      {/* travel devs */}
       <div className='ProyectContainer'>
         
         <div className='image-container'>
@@ -76,16 +142,16 @@ const Proyects = () => {
         {/* Div que contiene la información del proyecto */}
         <div className="project-info">
             
-          <div>
+          <div className='proyectsTextContain'>
             <h3 className='proyectNameDevs'>Travel Devs</h3>
-            <p className='proyectDevs'>{t('proyectos.descripcion')}:</p>
+            <p className='proyectDevs1'>{t('proyectos.descripcion')}:</p>
             <p className='proyectDevs'>{t('proyectos.descripcionDev')} </p>
             <p className='proyectDevs'>{t('proyectos.flujo')}</p>
             <p className='proyectDevs'>{t('proyectos.relevancias')}</p>
             <p className='proyectDevs'>{t('proyectos.responsive')}</p>
            
           </div>
-
+          <hr />
           <div className='travelTechContainer'>
 
             <h5 className='techTitle'>{t('proyectos.tecnologias')}:</h5>
