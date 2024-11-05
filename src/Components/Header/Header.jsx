c143f711c0e459e553ed67c4c4b223d1b5d196a6
LeonardoRiera <159967149+LeonardoRiera@users.noreply.github.com>
2024-11-04 22:51:47 -0300

    animacion de header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,55 +1,31 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { LanguageContext } from '../../Contexts/LanguageContext.jsx';
-import { useTranslation } from 'react-i18next';
-import { useSpring, animated } from 'react-spring';
+// Header.jsx
+
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 import './Header.css';
-import circulo from '../../image/circulo.png';
-import triangulo from '../../image/triangulo.png'  
-import caminante from '../../image/caminante.png'
-
-
+import triangulo from '../../image/triangulo.png';
 
 const Header = () => {
-  
-  const { t, i18n } = useTranslation();
-  const { language, setLanguage } = useContext(LanguageContext);  
-
-
-  const [scrollY, setScrollY] = useState(0);
-  // Actualiza el estado cuando haces scroll
-  useEffect(() => {
-    const handleScroll = () => {
-        setScrollY(window.scrollY);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  // Aplica la rotación basada en la posición de scroll
-  const styles = useSpring({
-    transform: `translateY(${scrollY * 0.2}px) rotate(${scrollY * 0.2}deg)` // Ajusta el valor para cambiar la velocidad de rotación
-  }); 
-
-
-
+ 
   return (
+    <div className="HeaderContainer">
 
-    <div className='HeaderContainer'>
-
-      {/* <img src={triangulo} alt="" className='red'  /> */}
-      
-      <animated.img
-                src={triangulo} 
-                alt="Triángulo"
-                style={styles}
-                className="red"
-       />
-
-
+      <div class="container">
     
-       
+        <div class="box">
+          <div class="card" id="front">LEONARDO</div>
+          <div class="card" id="back">RIERA</div>
+          <div class="card" id="left">FRONTEND</div>
+          <div class="card" id="right">DISEÑO</div>
+          <div class="card" id="top">DEVELOPER</div>
+          <div class="card" id="bottom">REACT</div>
+
+        </div>
+
+      </div>
+
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
